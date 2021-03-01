7ae3edbeb31179ba00347d638df314f5d4d6a6ac
Sharma <shrsharma@teksystems.com>
2021-03-01 21:33:35 +0530

    Add project files.

---
+++ StudentRecords/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StudentRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Record.Do();
+
+        }
+    }
+}
---
+++ StudentRecords/Record.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace StudentRecords
+{
+    class Record
+    {
+        public static void Do()
+        {
+            ReadLine();
+        }
+
+        private static void ReadLine()
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string filename = dir + "\\RecordsOfStudent.txt";
+            Console.WriteLine(filename);
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("File doesn't exist");
+            }
+            else
+            {
+                Console.WriteLine("File exists");
+                string[] contents = File.ReadAllLines(filename);
+                foreach (var content in contents)
+                {
+                    string[] data= content.Split(", ");
+                    Console.WriteLine("\n Name:"+data[0]+", Age:"+data[1]+", Dept:"+data[2]);
+              
+
+                }
+            }
+        }
+    }
+}
